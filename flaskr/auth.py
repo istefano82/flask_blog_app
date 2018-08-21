3ed125efc423b24879b1d9b2caf67f4f727d2578
ivaylo_stefanov82@yahoo.com <ivaylo_stefanov82@yahoo.com>
2018-08-21 22:53:59 +0300

    Fix a typo causing exception when trying to log with incorrect password.

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -39,28 +39,32 @@ def register():
     return render_template('auth/register.html')
 
 
+
 @bp.route('/login', methods=('GET', 'POST'))
 def login():
+    """Log in a registered user by adding the user id to the session."""
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
         db = get_db()
         error = None
         user = db.execute(
-            'SELECT * from user WHERE username ?', (username,)
+            'SELECT * FROM user WHERE username = ?', (username,)
         ).fetchone()
 
         if user is None:
-            error = 'Incorrent username.'
+            error = 'Incorrect username.'
         elif not check_password_hash(user['password'], password):
             error = 'Incorrect password.'
 
         if error is None:
+            # store the user id in a new session and return to the index
             session.clear()
             session['user_id'] = user['id']
             return redirect(url_for('index'))
 
         flash(error)
+
     return render_template('auth/login.html')
 
 
